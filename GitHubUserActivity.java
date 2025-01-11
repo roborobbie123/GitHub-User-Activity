7500d14e2cb443197bbb56dc7a64e2e92b9b1120
Robert Barrett <roborobbie123@gmail.com>
2025-01-11 13:31:37 -0500

    application get request to github api and reads in data to response

--- GitHubUserActivity.java
+++ GitHubUserActivity.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Scanner;
 
 public class GitHubUserActivity {
@@ -12,10 +16,35 @@ public class GitHubUserActivity {
                 username = username.replace("<", "").replace(">", "");
                 String path = "https://api.github.com/users/" + username + "/events";
                 System.out.println(path);
+                try {
+                    URL url = new URL(path);
+
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("GET");
+                    int responseCode = connection.getResponseCode();
+                    if(responseCode == HttpURLConnection.HTTP_OK) {
+                        System.out.println("valid");
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                        String inputLine;
+                        StringBuilder response = new StringBuilder();
+                        while ((inputLine = reader.readLine()) != null) {
+                            response.append(inputLine);
+                        }
+                        reader.close();
+                        System.out.println(response.toString());
+                        break;
+                    } else {
+                        System.out.println("GET request failed. Response code: " + responseCode);
+                    }
+                }
+                catch(Exception e) {
+                    System.out.println("could not connect to GitHub");
+                    break;
+                }
                 break;
             }
-
         }
+
 
 
     }
