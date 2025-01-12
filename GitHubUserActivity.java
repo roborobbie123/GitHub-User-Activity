f39cbe54f727e7086e0e265649dcdfe83c5243e6
Robert Barrett <roborobbie123@gmail.com>
2025-01-12 10:57:20 -0500

    correctly parses JSON and prints recent activity

--- GitHubUserActivity.java
+++ GitHubUserActivity.java
@@ -2,12 +2,17 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class GitHubUserActivity {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your username in this format: <username>");
+
         while(true) {
             String username = scanner.nextLine();
             if(username.charAt(0) != '<' && username.charAt(username.length() - 1) != '>') {
@@ -25,7 +30,6 @@ public class GitHubUserActivity {
                     connection.setRequestMethod("GET");
                     int responseCode = connection.getResponseCode();
                     if(responseCode == HttpURLConnection.HTTP_OK) {
-                        System.out.println("valid");
                         // gets input stream from URLConnection and converts it to character data.
                         BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
                         String inputLine;
@@ -34,8 +38,9 @@ public class GitHubUserActivity {
                         while ((inputLine = reader.readLine()) != null) {
                             response.append(inputLine);
                         }
+                        getActivities(response.toString());
                         reader.close();
-                        System.out.println(response.toString());
+                        //System.out.println(response);
                         connection.disconnect();
                         break;
                     } else {
@@ -49,8 +54,24 @@ public class GitHubUserActivity {
                 break;
             }
         }
+    }
+    public static void getActivities(String data) {
+        List<String> activities = new ArrayList<>();
+        String regex = "\\{\"id\":\\s*\"\\d+\",.*?\"type\":\\s*\"([^\"]+)\",.*?\"repo\":\\s*\\{.*?\"name\":\\s*\"roborobbie123/([^\"]+)\"";
+        ;
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(data);
+        while(matcher.find()) {
+            String activity = matcher.group(1);
+            String repo = matcher.group(2);
+            activities.add(activity + ":" + repo);
+        }
 
-
-
+        for(String activity : activities) {
+            System.out.println(activity);
+        }
+        if(activities.isEmpty()){
+            System.out.println("no activities");
+        }
     }
 }
